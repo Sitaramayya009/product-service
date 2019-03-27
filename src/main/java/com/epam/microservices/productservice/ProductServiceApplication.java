06afb7b5833cce4d8aa05a5502681d8c9d7c6a3c
Sitaramayya Chokkam <Sitaramayya_Chokkam@epam.com>
2019-03-27 10:41:37 +0530

    Added Feignclient anf hystrix related changes

--- src/main/java/com/epam/microservices/productservice/ProductServiceApplication.java
+++ src/main/java/com/epam/microservices/productservice/ProductServiceApplication.java
@@ -2,10 +2,14 @@ package com.epam.microservices.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
